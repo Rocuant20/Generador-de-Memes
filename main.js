c4030b94e260c5d0a89ae0e3798ee89e899b6908
valentin <valentinsarmiento13@hotmail.com>
2022-10-11 00:52:57 -0300

    avanzamos con modo claro/oscuro y ahorramos algo de codigo

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const sectionSidebars = document.getElementById(`section-sidebars`);
 const sectionImage = document.getElementById(`sidebar-image`);
 const sectionText = document.getElementById(`sidebar-text`);
 const headerTittle = document.getElementById(`encabezado`)
+const sideBard = document.getElementById(`sidebars`)
+
 
 buttonImage.addEventListener(`click`,(e) =>{
     e.preventDefault()
@@ -19,6 +21,9 @@ buttonText.addEventListener(`click`,(e)=>{
 buttonDarkMode.addEventListener(`click`,(e)=>{
     e.preventDefault()
     document.body.classList.toggle(`lightmode`)
+    headerTittle.classList.toggle(`ivorymode`)
+    sideBard.classList.toggle('yellowmode')
 })
+
 
 
