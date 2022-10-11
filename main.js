a7e44a4860f0c559a441268b4f349398a75f5f21
Federico Rocuant <federico.rocuant20@outlook.com>
2022-10-11 02:16:48 -0300

    trabajamos en la funcionalidad de la imagen y texto.

--- main.js
+++ main.js
@@ -4,8 +4,14 @@ const buttonDarkMode = document.getElementById(`button-darkMode`);
 const sectionSidebars = document.getElementById(`section-sidebars`);
 const sectionImage = document.getElementById(`sidebar-image`);
 const sectionText = document.getElementById(`sidebar-text`);
-const headerTittle = document.getElementById(`encabezado`)
-const sideBard = document.getElementById(`sidebars`)
+const headerTittle = document.getElementById(`encabezado`);
+const sideBard = document.getElementById(`sidebars`);
+const memeBox = document.getElementById(`meme`);
+const urlText = document.getElementById(`url-text`);
+const topText = document.getElementById(`texto-arriba`);
+const bottomText = document.getElementById(`texto-abajo`);
+const topURL = document.getElementById(`Texto-Superior`);
+const bottomURL = document.getElementById(`texto-inferior`);
 
 
 buttonImage.addEventListener(`click`,(e) =>{
@@ -24,6 +30,13 @@ buttonDarkMode.addEventListener(`click`,(e)=>{
     headerTittle.classList.toggle(`ivorymode`)
     sideBard.classList.toggle('yellowmode')
 })
+urlText.addEventListener(`input`, () =>{
+    memeBox.style.backgroundImage = `url("${urlText.value}")`
+})
+topURL.addEventListener(`input`,() =>{
+    topText.innerHTML = `<h1>${topURL.value}</h1>`
+})
 
+console.log(topURL)
 
 
